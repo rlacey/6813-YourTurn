a4c2dcdd840ac8c38512fd8c4599a98051d90eb1
Amruth Venkatraman <amruthv@mit.edu>
2013-04-15 20:52:40 -0400

    Added in grid layout for toy pictures and descriptions.

--- find.js
+++ find.js
@@ -4,15 +4,53 @@
  */
 
  var numRows=1;
- var capPerRow=5;
+ var capPerRow=4;
  var numPerRow={1:0}
 
 $(document).ready(function(){
+	var toy1= new toy(1,'badddddddddddddddddddddddddddddddddddddddddd','1-4','new','cat1','desc',"images/original.png");
+	addNewToy(toy1);
+	addNewToy(toy1);
+	addNewToy(toy1);
+	addNewToy(toy1);
+	addNewToy(toy1);
 
 });
 
-function addNewToy(rowNum){
-	var colToPut=numPerRow.1 + 1;
-	numPerRow.1 = colToPut;
-	$('#row'+numRows).
+function addNewToy(toy){
+	console.log('in add new toy')
+	if (numPerRow[numRows] + 1 <= capPerRow){
+		numPerRow[numRows] += 1;
+	}
+	else{
+		numRows+=1;
+		numPerRow[numRows]=1;
+	}
+	var col=numPerRow[numRows];
+	 
+	if ($('#row'+numRows).length==0){
+		var newRow = $('<div>', {id:'row'+numRows,class:'row-fluid toyRow'});
+		$('#toyWrapper').append(newRow);
+	}
+	var row= $('#row'+numRows);
+	var colDiv= $('<div>', {id:'row'+numRows+'col'+col,class:'span3 toyEntry'});
+	row.append(colDiv);
+
+
+	var pic= $('<img>',{src:toy.photo, class:'img-polaroid text-center'});
+	console.log(pic);
+	colDiv.append(pic);
+
+	var text=$('<p>',{class:'toyBrief', html:'<strong>Name</strong>:'+toy.name+'<br><strong>Condition</strong>:'+toy.condition});
+	colDiv.append(text);
+}
+
+function toy(id, name, ageRange, condition, categories, description, photo){
+	this.id=id;
+	this.name=name;
+	this.ageRange=ageRange;
+	this.categories=categories;
+	this.condition=condition;
+	this.description=description;
+	this.photo=photo;
 }
